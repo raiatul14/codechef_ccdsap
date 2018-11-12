30d7449a220e27f6814a6ff13282ca09b7372c1d
Atul Rai <17046074+raiatul14@users.noreply.github.com>
2018-11-12 10:59:16 +0530

    Create CNOTE.py

---
+++ foundation/arrays/CNOTE.py
@@ -0,0 +1,14 @@
+for _ in range(int(input())):
+    x,y,k,n=map(int, input('').split())
+    l=[]
+    flag=0
+    for i in range(n):
+        p,c=map(int,input('').split())
+        l.append([p,c])
+    for i in range(n):
+        if y+l[i][0]>=x and k>=l[i][1]:
+            flag=1
+            print('LuckyChef')
+            break
+    if flag==0:
+        print('UnluckyChef')
